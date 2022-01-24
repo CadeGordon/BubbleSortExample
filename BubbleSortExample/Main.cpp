fed45e6544b6a94539258e9b5400fe61edbecfcf
Cade Gordon <cadegordon093@gmail.com>
2022-01-24 16:56:59 -0600

    uncommented code

--- BubbleSortExample/Main.cpp
+++ BubbleSortExample/Main.cpp
@@ -55,66 +55,66 @@ int main()
 }
 
 
-//void insertionSort(int arr[], int size)
-//{
-//    
-//    
-//    int j = 0;
-//    int key = 0;
-//
-//    //iterate through the array
-//    for (int i = 1; i < size; i++)
-//    {
-//        //Sotre the current value of teh current index
-//        key = arr[i];
-//        //set j to be the previous index
-//        j = i - 1;
-//
-//        //while j hasnt reached the end of the list
-//        //and the value at the index of j is greater than they key
-//        while (j >= 0 && arr[j] > key)
-//        {
-//            //..move each elemtnt in the array over by 1
-//            arr[j + 1] = arr[j];
-//            j--;
-//
-//        }
-//        //set the index after j to be the key
-//        arr[j + 1] = key;
-//    }
-//
-//    
-//
-//
-// //for ( int i = 1; i < size; i++)
-// // set key to be the value at the current index
-// // set j to be the previous index
-// // start while loop
-// // while j is greather than or equal 0 adn the value at j is greater than key
-// // set collection at the index of j+1 to be the value at index of j
-// // decrement j
-// // end while loop
-// // set collection at the index of j+1 to be the key
-//}
-//
-//void printArray(int arr[], int size)
-//{
-//    int i;
-//    for (i = 0; i < size; i++)
-//       std::cout << arr[i] << " ";
-//    std::cout << std::endl;
-//}
-//
-//
-//int main()
-//{
-//    int arr[] = { 12, 11, 13, 5, 6 };
-//    int size = sizeof(arr) / sizeof(arr[0]);
-//
-//    insertionSort(arr, size);
-//    printArray(arr, size);
-//
-//    return 0;
-//}
+void insertionSort(int arr[], int size)
+{
+    
+    
+    int j = 0;
+    int key = 0;
+
+    //iterate through the array
+    for (int i = 1; i < size; i++)
+    {
+        //Sotre the current value of teh current index
+        key = arr[i];
+        //set j to be the previous index
+        j = i - 1;
+
+        //while j hasnt reached the end of the list
+        //and the value at the index of j is greater than they key
+        while (j >= 0 && arr[j] > key)
+        {
+            //..move each elemtnt in the array over by 1
+            arr[j + 1] = arr[j];
+            j--;
+
+        }
+        //set the index after j to be the key
+        arr[j + 1] = key;
+    }
+
+    
+
+
+ //for ( int i = 1; i < size; i++)
+ // set key to be the value at the current index
+ // set j to be the previous index
+ // start while loop
+ // while j is greather than or equal 0 adn the value at j is greater than key
+ // set collection at the index of j+1 to be the value at index of j
+ // decrement j
+ // end while loop
+ // set collection at the index of j+1 to be the key
+}
+
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+       std::cout << arr[i] << " ";
+    std::cout << std::endl;
+}
+
+
+int main()
+{
+    int arr[] = { 12, 11, 13, 5, 6 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, size);
+    printArray(arr, size);
+
+    return 0;
+}
 
 
